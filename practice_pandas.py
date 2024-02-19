8ff28dcfa9a6b0333e87b4c9a8e1f98466f662c9
seomar <seomar2022@gmail.com>
2024-02-19 09:15:04 +0900

    feat: Pandas

--- practice_pandas.py
+++ practice_pandas.py
@@ -1,8 +1,16 @@
+#pip install pandas
+#pip install Pyarrow
 import pandas as pd
 
 #data = pd.read_excel("C:/study/CS codes.xlsx")
 #print(data)
 
 cs_codes = pd.read_excel("C:/study/CS codes.xlsx", usecols=[1])
-print(cs_codes)
-print(cs_codes.values.tolist)
+#print(cs_codes)
+list = cs_codes.values.tolist()
+print(list[0])
+for i in list:
+    print(int(i))
+    print(type(int(i)))
+    print("-----")
+print(type(list[0]))
