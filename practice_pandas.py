8f61b26d934c681e5555003d3ec7eb036eb828f0
seomar <seomar2022@gmail.com>
2024-02-23 23:56:55 +0900

    feat: Pandas

--- practice_pandas.py
+++ practice_pandas.py
@@ -17,8 +17,14 @@ cs_codes = cs_codes.dropna().values.tolist()
 
 cs_codes = [int(float(str(x).split('.')[0])) for sublist in cs_codes for x in sublist]
 
-print(cs_codes)
+#print(cs_codes)
 #print(type(converted_list[0]))
 
 
 # print(type(list[0]))
+
+coupon_code = pd.read_excel("C:/study/CS codes.xlsx", usecols=[1], sheet_name="coupon")
+print(coupon_code.shape)
+print(coupon_code.loc[0])
+print(coupon_code.tail(1))
+#print(coupon_code.values.tolist()[1])
