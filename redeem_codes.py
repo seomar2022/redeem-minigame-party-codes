14f340ac35ac80e5649f47eee1126b323cd6ce12
seomar <seomar2022@gmail.com>
2024-02-27 23:12:33 +0900

    feat: Loop

--- redeem_codes.py
+++ redeem_codes.py
@@ -3,7 +3,6 @@ import time
 from selenium.webdriver.common.by import By
 import pandas as pd  #pip install pandas
 
-
 driver = webdriver.Chrome()
 
 driver.get("https://coupon.withhive.com/720")
@@ -22,11 +21,6 @@ cs_codes = cs_codes.dropna().values.tolist()
 #[105548.0] -> 105548
 cs_codes = [int(float(str(x).split('.')[0])) for sublist in cs_codes for x in sublist]
 
-#select a server
-driver.find_element(By.CSS_SELECTOR, "#HIVEcoupon > div.content > div > div.input_box.server_list_area.show > div.select_wrap > button").click()
-driver.find_element(By.CSS_SELECTOR, "#HIVEcoupon > div.content > div > div.input_box.server_list_area.show > div.select_wrap > ul > li > button").click()
-
-
 coupon_code = pd.read_excel("C:/study/CS codes.xlsx", usecols=[1], sheet_name="coupon")
 
 #search last cell that is inserted data
@@ -34,30 +28,36 @@ rows, cols = coupon_code.shape
 last_row_index = rows - 1
 last_col_index = cols - 1
 
-#input CS code and coupon code 
-driver.find_element(By.CSS_SELECTOR, "#cs_code").send_keys(f"{cs_codes[2]}")
-driver.find_element(By.CSS_SELECTOR, "#coupon_code").send_keys(coupon_code.iat[last_row_index, last_col_index])
-driver.find_element(By.CSS_SELECTOR, "#HIVEcoupon > div.content > div > button").click()
-time.sleep(5)
+for cs_code in cs_codes:
+        
+    #select a server
+    driver.find_element(By.CSS_SELECTOR, "#HIVEcoupon > div.content > div > div.input_box.server_list_area.show > div.select_wrap > button").click()
+    driver.find_element(By.CSS_SELECTOR, "#HIVEcoupon > div.content > div > div.input_box.server_list_area.show > div.select_wrap > ul > li > button").click()
 
-#select a server again
-driver.find_element(By.CSS_SELECTOR, "body > div.pop_wrap.server.coupon_server_lyr > div > ul > li > label > span").click()
-driver.find_element(By.CSS_SELECTOR, "body > div.pop_wrap.server.coupon_server_lyr > div > div.btns > button.btn_confirm").click()
+    #input CS code and coupon code 
+    driver.find_element(By.CSS_SELECTOR, "#cs_code").send_keys(cs_code)
+    driver.find_element(By.CSS_SELECTOR, "#coupon_code").send_keys(coupon_code.iat[last_row_index, last_col_index])
+    driver.find_element(By.CSS_SELECTOR, "#HIVEcoupon > div.content > div > button").click()
 
-time.sleep(5)
+    #select a server again
+    driver.find_element(By.CSS_SELECTOR, "body > div.pop_wrap.server.coupon_server_lyr > div > ul > li > label > span").click()
+    driver.find_element(By.CSS_SELECTOR, "body > div.pop_wrap.server.coupon_server_lyr > div > div.btns > button.btn_confirm").click()
 
-#result(1) -> redeem the code successfully
-#result(2) -> the codes was already used
+    #read the message of alert
+    result = driver.find_element(By.CSS_SELECTOR, "#layer_msg").text
 
 
-# 
-result = driver.find_element(By.CSS_SELECTOR, "#layer_msg").text
+    #result(1) -> redeem the code successfully
 
-if result.find("!") == -1:
-    print("이미등록된 코드")
-print(result)
+    #result(2) -> the codes was already used
+    already_used = []
+    if result.find("!") == -1: #when the message contain a letter '!', it means redeeming was completed.
+        already_used.append(cs_code)
 
-driver.find_element(By.CSS_SELECTOR, "#layer_close_btn").click()
-time.sleep(5)
+    #click the close button
+    driver.find_element(By.CSS_SELECTOR, "#layer_close_btn").click()
+    
+    #refre a webpage
+    driver.refresh()
 
 driver.quit()
